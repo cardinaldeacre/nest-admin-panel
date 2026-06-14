f0cd4bd52d02e9b03b8b6aebce80acdb8730a1d5
cardinaldeacre <cardinaldeacre@gmail.com>
2026-06-14 14:20:58 +0700

    add user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { Connection, MySQLConnection, PostgreSQLConnection } from './connection/connection';
+import { mailService, MailService } from './mail/mail.service';
+import { UserRepository } from './user-repository/user-repository';
 
 @Module({
     controllers: [UserController],
-    providers: [UserService]
+    providers: [
+        UserService, 
+        {
+            provide: Connection,
+            useClass: process.env.DB_CONNECTION === 'mysql' ? MySQLConnection : PostgreSQLConnection,
+        },{
+
+            provide: MailService,
+            useValue: mailService,
+        }, UserRepository
+    ]
 })
 export class UserModule {}
